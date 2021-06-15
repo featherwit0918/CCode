06adf24402e65e326e317abc4506f90c5a1374cd
张杰 <featherwit0918@163.com>
2021-06-15 12:58:38 +0800

    初始化C/C++环境

---
+++ CBase/cmake-build-debug/CMakeFiles/CBase.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CBase"
+  "CBase.pdb"
+  "CMakeFiles/CBase.dir/hello.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CBase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
